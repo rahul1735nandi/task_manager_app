7db8438cb69c31a3a2bc3a9cbac34012645a2485
rahul1735nandi <rahul1735nandi@gmail.com>
2025-03-28 19:21:04 +0530

    removed constants file

--- lib/constants.dart
+++
@@ -1,3 +0,0 @@
-const String baseUrl = "https://parseapi.back4app.com";
-const String appId = "dJ6nwRvROxnFcNWj9YMsKqJP7pitYBhInymqKOlv";
-const String restAPIkey = "Mjrqso8AEtyZkLWAzqkPVwYHZCe4jOu5bD5cGRoZ";
